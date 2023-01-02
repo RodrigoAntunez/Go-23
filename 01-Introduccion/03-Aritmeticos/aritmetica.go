87eac94176b7566a58c8f7c518d214f4a75aadfb
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-02 14:02:33 -0300

    Practica de sintaxis

---
+++ 01-Introduccion/03-Aritmeticos/aritmetica.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 3
+	b := 2
+
+	// Suma
+	result := a + b
+	fmt.Println("Suma:", result)
+
+	// Resta
+	result = a - b
+	fmt.Println("Resta:", result)
+
+	// Multiplicación
+	result = a * b
+	fmt.Println("Multiplicación:", result)
+
+	// División
+	result = a / b
+	fmt.Println("División:", result)
+}
