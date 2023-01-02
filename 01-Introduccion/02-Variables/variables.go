2b4789e5c7b3c3d61eb4ce492d2f39cd61e6e548
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-02 13:59:42 -0300

    Parte1

---
+++ 01-Introduccion/02-Variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	//Formas de declarar variables
+
+	var nombre1 string
+	nombre1 = "Rodri"
+
+	var nombre2 string = "Agustin"
+
+	edad := 23
+
+	println(nombre1, nombre2, edad)
+
+}
