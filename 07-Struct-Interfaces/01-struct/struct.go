6a6044ec4430a6343604e8087b7f458649168898
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-06 22:29:32 -0300

    POO Metodos

--- 07-Struct-Interfaces/01-struct/struct.go
+++ 07-Struct-Interfaces/01-struct/struct.go
@@ -8,20 +8,35 @@ type Persona struct {
 	Edad   int
 }
 
+//Crear metodos desde la estructura persona
+
+//Metodo para imprimir los datos de la persona
+func (p *Persona) imprimir() {
+	fmt.Printf("Nombre: %s\nEdad: %d\n", p.Nombre, p.Edad)
+}
+
+// Metodo para modicar el nombre de la persona y no hacerlo de forma explicita
+func (p *Persona) modificarNombre(nombre string) {
+	p.Nombre = nombre
+}
+
 //Crear objetos desde la estructura persona
 
 func main() {
 	persona1 := Persona{"Juan", 20}
-	fmt.Println(persona1)
+	//fmt.Println(persona1)
+	persona1.imprimir()
 
 	//Si queremos moficar el valor de una propiedad
-	persona1.Nombre = "Pedro"
-
-	fmt.Println(persona1)
+	//persona1.Nombre = "Pedro"
+	persona1.modificarNombre("Rodri")
+	persona1.imprimir()
+	//fmt.Println(persona1)
 
 	persona2 := Persona{
 		Nombre: "Maria",
 		Edad:   30,
 	}
-	fmt.Println(persona2)
+	//fmt.Println(persona2)
+	persona2.imprimir()
 }
