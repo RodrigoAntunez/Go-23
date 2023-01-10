bdf1c9e76b07a77b9416e84fe30a015c389df6c8
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-10 11:50:45 -0300

    Interfaces

---
+++ 07-Struct-Interfaces/04-Interfaces/RetoInterface.go
@@ -0,0 +1,46 @@
+package main
+
+type Cuadrado interface {
+	area() float64
+	perimetro() float64
+}
+
+type Circulo interface {
+	area() float64
+	perimetro() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func (s square) Area() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (s square) Perimetro() float64 {
+	return s.sideLength * 4
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) Area() float64 {
+	return 3.14 * c.radius * c.radius
+
+}
+
+func (c circle) Perimetro() float64 {
+	return 2 * 3.14 * c.radius
+}
+
+func main() {
+	mySquare := square{sideLength: 10}
+	area := mySquare.Area()
+	perimetro := mySquare.Perimetro()
+
+	myCircle := circle{radius: 10}
+	area := myCircle.Area()
+	perimetro := myCircle.Perimetro()
+}
