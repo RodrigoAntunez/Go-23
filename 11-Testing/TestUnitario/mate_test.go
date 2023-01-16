9708fe7e987d585425166335a8698582c5dfb895
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-16 10:43:44 -0300

    Test Convencional

--- 11-Testing/TestUnitario/mate_test.go
+++ 11-Testing/TestUnitario/mate_test.go
@@ -2,9 +2,30 @@ package testunitario
 
 import "testing"
 
-func TestSuma(t *testing.T) {
+//TEST UNITARIO
+/*func TestSuma(t *testing.T) {
 	total := Suma(5, 5)
 	if total != 10 {
 		t.Errorf("Suma de 5 + 5 debe ser 10, pero fue %d", total)
+	}
+} */
+
+//TEST DE TABLAS (Convencional)
+
+func TestSuma(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}{
+		{1, 2, 3},
+		{2, 2, 4},
+		{25, 25, 50},
+	}
+	for _, item := range tabla {
+		total := Suma(item.a, item.b)
+		if total != item.c {
+			t.Errorf("Suma de %d + %d debe ser %d, pero fue %d", item.a, item.b, item.c, total)
+		}
 	}
 }
