544cb7b6f2cf22fbfe6b787634617a999e256a0e
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-17 10:05:32 -0300

    Router y Hangler

--- 12-WordSpace/GoWeb/01-Introduccion/GoWeb.go
+++ 12-WordSpace/GoWeb/01-Introduccion/GoWeb.go
@@ -6,10 +6,24 @@ import (
 	"net/http"
 )
 
+// Handler:
+func Hola(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Hola Mundo desde un Handler")
+}
+
 func main() {
+
+	//Router
+	http.HandleFunc("/", Hola)
+
 	//Crear un servidor web
 	//http.ListenAndServe("localhost:8080", nil)
-	fmt.Println("El Servidor web esta en el puerto http://localhost:8080")
-	fmt.Println("Run server: http://localhost:8080/")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	fmt.Println("El Servidor web esta en el puerto http://localhost:3000")
+	fmt.Println("Run server: http://localhost:3000/")
+	log.Fatal(http.ListenAndServe("localhost:3000", nil))
+
+	// Router y Handler
+	//Router: Es el encargado de recibir las peticiones y enviarlas a un Handler
+	//Handler: Es el encargado de procesar la petición y devolver una respuesta(son funciones que responden a una ruta)
+
 }
