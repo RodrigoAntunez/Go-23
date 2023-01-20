f0279d55f57662f958d70fbcea5d10da75731a7c
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-20 20:00:37 -0300

    Prueba

--- 12-WordSpace/GoWeb/01-Introduccion/Prueba/servidor.go
+++ 12-WordSpace/GoWeb/01-Introduccion/Prueba/servidor.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-func Hola(w http.ResponseWriter, r *http.Request){
+func Hola(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hola mundo")
 }
 
-func Chau(w http.ResponseWriter, r *http.Request){
+func Chau(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Chau mundo desde funcion")
 }
 
-
-func main()  {
+func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Hola)
-	mux.HandleFunc("/1", Chau)
+	mux.HandleFunc("/chau", Chau)
 
 	server := &http.Server{
 		Addr:    "localhost:3000",
 		Handler: mux,
-	}	
+	}
 
 	fmt.Println("Servidor corriendo en http://localhost:3000")
 
