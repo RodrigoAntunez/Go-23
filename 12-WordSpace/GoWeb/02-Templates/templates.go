77c1bf1b9e3c42826530bc2d1b9d889d7ff77451
RodrigoAntunez <antunezrodrigo.a@gmail.com>
2023-01-19 23:23:27 -0300

    Templates html

--- 12-WordSpace/GoWeb/02-Templates/templates.go
+++ 12-WordSpace/GoWeb/02-Templates/templates.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
 
+// Handler de la pagina principal
+// Esta funcion se encarga de renderizar el template index.html
+func Index(rw http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hola mundo")
+	template, err := template.ParseFiles("index.html")
+	if err != nil {
+		panic(err)
+	} else {
+		template.Execute(rw, nil)
+	}
+}
+
+func main() {
+	//Mux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Index)
+
+	//Servidor
+	server := &http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
+
+	fmt.Println("Servidor corriendo en http://localhost:3000")
+
+	log.Fatal(server.ListenAndServe())
 }
